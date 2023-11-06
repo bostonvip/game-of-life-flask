2deabb2c54d7c38444eb923c5f045af7f30f0bcf
Vasyl Pominov <bostonvip@outlook.com>
2023-11-05 22:06:58 -0500

    added code for the grid and two buttons

--- life-app.py
+++ life-app.py
@@ -1,11 +1,14 @@
 # app.py
 from flask import Flask, render_template
 
+COLONY_NUMBER_OF_ROWS = 30
+COLONY_NUMBER_OF_COLS = 50
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', num_of_rows = COLONY_NUMBER_OF_ROWS, num_of_cols = COLONY_NUMBER_OF_COLS)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,4 +1,17 @@
 // static/script.js
-function clickMe() {
-    alert('Button clicked!');
-}
+window.onload = function() {
+    var startButton = document.getElementById('start-button');
+    var clearButton = document.getElementById('clear-button');
+
+    startButton.addEventListener('click', function() {
+        // Add your "Start" button functionality here
+        alert('Start button clicked');
+    });
+
+    clearButton.addEventListener('click', function() {
+        // Add your "Clear" button functionality here
+        // console.log('Clear button clicked');
+        alert('Clear Button clicked!');
+    });
+};
+
