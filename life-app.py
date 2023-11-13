a2e7bc9a5ff3d80bb28e96afaf50796495bb3ee7
Vasyl Pominov <bostonvip@outlook.com>
2023-11-12 21:55:11 -0500

    button event handlers added in python; gen label

--- life-app.py
+++ life-app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 COLONY_NUMBER_OF_ROWS = 30
 COLONY_NUMBER_OF_COLS = 50
@@ -9,6 +9,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html', num_of_rows = COLONY_NUMBER_OF_ROWS, num_of_cols = COLONY_NUMBER_OF_COLS)
+
+@app.route('/start', methods=['POST'])
+def start_btn():
+    # Your "Start" button functionality here
+    # return jsonify({'result': 'Start button clicked!!!'})
+    new_generation = 1  # Replace this with your actual new generation number
+    return jsonify({'generation': new_generation})
+
+@app.route('/clear', methods=['POST'])
+def clear_btn():
+    # Your "Clear" button functionality here
+    return jsonify({'result': 'Clear button clicked!!!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,17 +1,36 @@
 // static/script.js
-window.onload = function() {
-    var startButton = document.getElementById('start-button');
-    var clearButton = document.getElementById('clear-button');
 
-    startButton.addEventListener('click', function() {
-        // Add your "Start" button functionality here
-        // alert('Start button clicked');
+// window.onload = function() {
+//     var startButton = document.getElementById('start-button');
+//     var clearButton = document.getElementById('clear-button');
+
+//     startButton.addEventListener('click', function() {
+//         // Add your "Start" button functionality here
+//         // alert('Start button clicked');
+//     });
+
+//     clearButton.addEventListener('click', function() {
+//         // Add your "Clear" button functionality here
+//         // console.log('Clear button clicked');
+//         // alert('Clear Button clicked!');
+//     });
+// };
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('start-button').addEventListener('click', () => {
+        fetch('/start', {method: 'POST'})
+            .then(response => response.json())
+            // .then(data => console.log(data.result))
+            .then(data => {
+                console.log('New generation:', data.generation);
+                document.getElementById('generation-label').textContent = 'Generation: ' + data.generation;
+            });
     });
 
-    clearButton.addEventListener('click', function() {
-        // Add your "Clear" button functionality here
-        // console.log('Clear button clicked');
-        // alert('Clear Button clicked!');
+    document.getElementById('clear-button').addEventListener('click', () => {
+        fetch('/clear', {method: 'POST'})
+            .then(response => response.json())
+            .then(data => console.log(data.result));
     });
-};
+});
 
